72ad5e2eb1a7cc56dd32683f6175e1e10990aed9
M4teoo <mateosolino1@gmail.com>
2024-01-09 19:29:44 +0100

    Add assets track.tmx

---
+++ assets/grass.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="grass" tilewidth="20" tileheight="20" tilecount="225" columns="15">
+ <image source="grass.png" width="303" height="312"/>
+</tileset>
---
+++ assets/sand.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="sand" tilewidth="5" tileheight="5" tilecount="4485" columns="69">
+ <image source="sand.png" width="347" height="325"/>
+</tileset>
