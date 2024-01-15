9801339b4b1e079538d2e0492d4937a63b699706
mateosolinho <mateosolino1@gmail.com>
2024-01-15 18:18:31 +0100

    Añadidos eventos de los botones (Falta optimizacion)

--- core/src/com/mygdx/game/Entities/Car.java
+++ core/src/com/mygdx/game/Entities/Car.java
@@ -1,74 +1,77 @@
-package com.mygdx.game.Entities;
-
-import com.badlogic.gdx.math.Vector2;
-import com.mygdx.game.BodyHolder;
-import com.mygdx.game.Tools.MapLoader;
-
-public class Car extends BodyHolder {
-    // Variables para indicar la dirección de conducción
-    public static int DRIVE_DIRECTION_NONE = 0;
-    public static int DRIVE_DIRECTION_FORWARD = 1;
-    public static int DRIVE_DIRECTION_BACKWARD = 2;
-
-
-    // Variables para indicar hacia donde es el giro
-    public  static int TURN_DIRECTION_NONE = 0;
-    public  static int TURN_DIRECTION_LEFT = 1;
-    public  static int TURN_DIRECTION_RIGHT = 2;
-
-    // Velocidad de conducción
-    private static float DRIVE_SPEED = 120.0f;
-    // Velocidad de giro
-    private static float TURN_SPEED = 2.0f;
-    // Velocidad máxima
-    private static float MAX_SPEED = 35.0f;
-
-    private static float LINEAR_DAMPING = 0.5f;
-    private static float RESTITUTION = 0.5f;
-
-
-    // Variable para comprobar la dirección de conducción del objeto
-    private int mDriveDirection = DRIVE_DIRECTION_NONE;
-    // Variable para comprobar la direccion de giro del objeto
-    private int mTurnDirection = TURN_DIRECTION_NONE;
-
-
-
-
-
-    public Car(MapLoader mapLoader) {
-        super(mapLoader.getPlayer());
-        getBody().setLinearDamping(LINEAR_DAMPING);
-        getBody().getFixtureList().get(0).setRestitution(RESTITUTION);
-    }
-
-    private void processInput() {
-        Vector2 baseVector = new Vector2(0,0);
-
-        if (mTurnDirection == TURN_DIRECTION_RIGHT){
-            getBody().setAngularVelocity(-TURN_SPEED);
-        } else if (mTurnDirection == TURN_DIRECTION_LEFT) {
-            getBody().setAngularVelocity(TURN_SPEED);
-        } else if (mTurnDirection == TURN_DIRECTION_NONE  && getBody().getAngularVelocity() != 0) {
-            getBody().setAngularVelocity(0.0f);
-        }
-
-        if (mDriveDirection == DRIVE_DIRECTION_FORWARD){
-            baseVector.set(0, DRIVE_SPEED);
-        } else if (mDriveDirection == DRIVE_DIRECTION_BACKWARD){
-            baseVector.set(0, -DRIVE_SPEED);
-        }
-
-        // Comprueba si hay una entrada activa para avanzar o retroceder y la velocidad actual del objeto es menor que la velocidad máxima
-        if (!baseVector.isZero() && getBody().getLinearVelocity().len() < MAX_SPEED){
-            // Aplica una fuerta en el centro del objeto
-            getBody().applyForceToCenter(getBody().getWorldVector(baseVector), true);
-        }
-    }
-
-    @Override
-    public void update(float delta) {
-        super.update(delta);
-        processInput();
-    }
-}
+//package com.mygdx.game.Entities;
+//
+//import com.badlogic.gdx.math.Vector2;
+//import com.mygdx.game.BodyHolder;
+//import com.mygdx.game.Tools.MapLoader;
+//
+//public class Car extends BodyHolder {
+//    // Variables para indicar la dirección de conducción
+//    public static int DRIVE_DIRECTION_NONE = 0;
+//    public static int DRIVE_DIRECTION_FORWARD = 1;
+//    public static int DRIVE_DIRECTION_BACKWARD = 2;
+//
+//
+//    // Variables para indicar hacia donde es el giro
+//    public  static int TURN_DIRECTION_NONE = 0;
+//    public  static int TURN_DIRECTION_LEFT = 1;
+//    public  static int TURN_DIRECTION_RIGHT = 2;
+//
+//    // Velocidad de conducción
+////    private static float DRIVE_SPEED = 120.0f;
+//    private static float DRIVE_SPEED = 500.0f;
+//    // Velocidad de giro
+////    private static float TURN_SPEED = 2.0f;
+//    private static float TURN_SPEED = 6.0f;
+//    // Velocidad máxima
+////    private static float MAX_SPEED = 35.0f;
+//    private static float MAX_SPEED = 65.0f;
+//
+//    private static float LINEAR_DAMPING = 0.5f;
+//    private static float RESTITUTION = 0.5f;
+//
+//
+//    // Variable para comprobar la dirección de conducción del objeto
+//    private int mDriveDirection = DRIVE_DIRECTION_NONE;
+//    // Variable para comprobar la direccion de giro del objeto
+//    private int mTurnDirection = TURN_DIRECTION_NONE;
+//
+//
+//
+//
+//
+//    public Car(MapLoader mapLoader) {
+//        super(mapLoader.getPlayer());
+//        getBody().setLinearDamping(LINEAR_DAMPING);
+//        getBody().getFixtureList().get(0).setRestitution(RESTITUTION);
+//    }
+//
+////    private void processInput() {
+////        Vector2 baseVector = new Vector2(0,0);
+////
+////        if (mTurnDirection == TURN_DIRECTION_RIGHT){
+////            getBody().setAngularVelocity(-TURN_SPEED);
+////        } else if (mTurnDirection == TURN_DIRECTION_LEFT) {
+////            getBody().setAngularVelocity(TURN_SPEED);
+////        } else if (mTurnDirection == TURN_DIRECTION_NONE  && getBody().getAngularVelocity() != 0) {
+////            getBody().setAngularVelocity(0.0f);
+////        }
+////
+////        if (mDriveDirection == DRIVE_DIRECTION_FORWARD){
+////            baseVector.set(0, DRIVE_SPEED);
+////        } else if (mDriveDirection == DRIVE_DIRECTION_BACKWARD){
+////            baseVector.set(0, -DRIVE_SPEED);
+////        }
+////
+////        // Comprueba si hay una entrada activa para avanzar o retroceder y la velocidad actual del objeto es menor que la velocidad máxima
+////        if (!baseVector.isZero() && getBody().getLinearVelocity().len() < MAX_SPEED){
+////            // Aplica una fuerta en el centro del objeto
+////            getBody().applyForceToCenter(getBody().getWorldVector(baseVector), true);
+////        }
+////    }
+//
+//    @Override
+//    public void update(float delta) {
+//        super.update(delta);
+////        processInput();
+//    }
+//}
--- core/src/com/mygdx/game/Screens/PlayScreen.java
+++ core/src/com/mygdx/game/Screens/PlayScreen.java
@@ -5,16 +5,22 @@ import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
 import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.viewport.FillViewport;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.mygdx.game.Tools.MapLoader;
-import com.mygdx.game.UI.ButtonController;
 
 // https://www.iforce2d.net/b2dtut/top-down-car <- Documento de explicación de las físicas 2d
 public class PlayScreen implements Screen {
@@ -34,16 +40,24 @@ public class PlayScreen implements Screen {
     // Deceleración del vehiculo al deslizarse o girar
     private static float DRIFT = 0.99f;
     // Velocidad de conducción
-    private static float DRIVE_SPEED = 120.0f;
+    private static float DRIVE_SPEED = 400.0f;
+//    private static float DRIVE_SPEED = 120.0f;
     // Velocidad de giro
-    private static float TURN_SPEED = 2.0f;
+    private static float TURN_SPEED = 4.0f;
+//    private static float TURN_SPEED = 2.0f;
     // Velocidad máxima
-    private static float MAX_SPEED = 35.0f;
+    private static float MAX_SPEED = 80.0f;
+//    private static float MAX_SPEED = 35.0f;
 
     // Variable para comprobar la dirección de conducción del objeto
     private int driveDirection = DRIVE_DIRECTION_NONE;
     // Variable para comprobar la direccion de giro del objeto
     private int turnDirection = TURN_DIRECTION_NONE;
+
+    private ImageButton imageButtonDerecha;
+    private ImageButton imageButtonIzquierda;
+    private ImageButton imageButtonArriba;
+    private ImageButton imageButtonAbajo;
 
     private Stage stage;
     private SpriteBatch batch;
@@ -55,6 +69,9 @@ public class PlayScreen implements Screen {
     private Vector2 GRAVITY = new Vector2(0,0);
     private float ZOOM = 6f;
     private MapLoader mapLoader;
+
+    int tick=30;
+    long ttotal=0;
 
     public PlayScreen(){
         batch = new SpriteBatch();
@@ -70,18 +87,44 @@ public class PlayScreen implements Screen {
 
         // Establece el amortiguamiento lineal del objeto player
         player.setLinearDamping(0.5f);
+        createButtons();
     }
     @Override
     public void show() {
-        ButtonController buttonController = new ButtonController(stage,camera, player, this);
-        stage = buttonController.getStage();
+
     }
+
+    Vector2 baseVector;
 
     @Override
     public void render(float delta) {
+        baseVector = new Vector2(0,0);
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+
         handleInput();
+
+        ;
+        if (System.currentTimeMillis()-ttotal>tick) {
+            if (isUpPressed) {
+               driveDirection = DRIVE_DIRECTION_FORWARD;
+            } else if (isDownPressed) {
+                driveDirection = DRIVE_DIRECTION_BACKWARD;
+            } else {
+                driveDirection = DRIVE_DIRECTION_NONE;
+            }
+
+            if (isRightPressed) {
+                turnDirection = TURN_DIRECTION_RIGHT;
+            } else if (isLeftPressed) {
+                turnDirection = TURN_DIRECTION_LEFT;
+            } else {
+                turnDirection = TURN_DIRECTION_NONE;
+            }
+            ttotal=System.currentTimeMillis();
+        }
+
         processInput();
         update(delta);
         handleDrift();
@@ -100,6 +143,11 @@ public class PlayScreen implements Screen {
         Vector2 lateralSpeed = getLateralVelocity();
         player.setLinearVelocity(forwardSpeed.x + lateralSpeed.x * DRIFT, forwardSpeed.y + lateralSpeed.y * DRIFT);
     }
+
+    boolean isUpPressed = false;
+    boolean isDownPressed = false;
+    boolean isRightPressed = false;
+    boolean isLeftPressed = false;
 
     private void processInput() {
         Vector2 baseVector = new Vector2(0,0);
@@ -122,6 +170,7 @@ public class PlayScreen implements Screen {
         if (!baseVector.isZero() && player.getLinearVelocity().len() < MAX_SPEED){
             // Aplica una fuerta en el centro del objeto
             player.applyForceToCenter(player.getWorldVector(baseVector), true);
+            Gdx.app.log("Siiiii" , "APLICANDO LA FUERZA");
         }
     }
 
@@ -156,7 +205,67 @@ public class PlayScreen implements Screen {
         return new Vector2(a * v.x , a * v.y);
     }
     private void handleInput() {
-        // TODO cuando funcione el programa cambiarlo a los botones en pantalla con EventListener
+        // -------------------------------------------------------------------------------------
+        imageButtonArriba.addListener(new InputListener(){
+            @Override
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                Gdx.app.log("pulso2", "true");
+                isUpPressed = true;
+                return true;
+            }
+
+            @Override
+            public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
+                isUpPressed = false;
+                Gdx.app.log("pulso2", "false");
+                super.touchUp(event, x, y, pointer, button);
+            }
+        });
+
+        imageButtonAbajo.addListener(new InputListener(){
+            @Override
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                isDownPressed = true;
+                return true;
+            }
+
+            @Override
+            public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
+                isDownPressed = false;
+                super.touchUp(event, x, y, pointer, button);
+            }
+        });
+
+        imageButtonDerecha.addListener(new InputListener(){
+            @Override
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                Gdx.app.log("pulso2", "true");
+                isRightPressed = true;
+                return true;
+            }
+
+            @Override
+            public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
+                isRightPressed = false;
+                Gdx.app.log("pulso2", "false");
+                super.touchUp(event, x, y, pointer, button);
+            }
+        });
+
+        imageButtonIzquierda.addListener(new InputListener(){
+            @Override
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                isLeftPressed = true;
+                return true;
+            }
+
+            @Override
+            public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
+                isLeftPressed = false;
+                super.touchUp(event, x, y, pointer, button);
+            }
+        });
+// -------------------------------------------------------------------------------------
         if (Gdx.input.isKeyPressed(Input.Keys.UP)){
             driveDirection = DRIVE_DIRECTION_FORWARD;
         } else if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
@@ -176,13 +285,10 @@ public class PlayScreen implements Screen {
         if (Gdx.input.isKeyPressed(Input.Keys.ESCAPE)){
             Gdx.app.exit();
         }
-
     }
 
     private void draw() {
-        // Se convierten las coordenadas del mundo en coordenadas de pantalla
         batch.setProjectionMatrix(camera.combined);
-        // Dibuja esos elementos en pantalla
         b2rd.render(world, camera.combined);
     }
 
@@ -221,5 +327,72 @@ public class PlayScreen implements Screen {
         b2rd.dispose();
         mapLoader.dispose();
         stage.dispose();
+    }
+
+    public void createButtons() {
+        final Vector2 baseVector = new Vector2(0,0);
+
+        // Crea una nueva instancia del Stage usando un viewport basado en pantalla (pixeles)
+        stage = new Stage(new ScreenViewport());
+        // Establece el Stage como el procesador de entrada principal (Gestión de eventos, click, toques en pantalla...)
+        Gdx.input.setInputProcessor(stage);
+
+        // Se le asgina una textura a cada botón
+        Texture buttonTextureDerecha = new Texture(Gdx.files.internal("UI/flecha-derecha.png"));
+        Texture buttonTextureIzquierda = new Texture(Gdx.files.internal("UI/flecha-izquierda.png"));
+        Texture buttonTextureArriba = new Texture(Gdx.files.internal("UI/flecha-arriba.png"));
+        Texture buttonTextureAbajo = new Texture(Gdx.files.internal("UI/flecha-abajo.png"));
+
+        // Creación de cada botón y asignación de la textura y el estilo
+        ImageButton.ImageButtonStyle styleDerecha = new ImageButton.ImageButtonStyle();
+        styleDerecha.imageUp = new TextureRegionDrawable(new TextureRegion(buttonTextureDerecha));
+        imageButtonDerecha = new ImageButton(styleDerecha);
+
+        ImageButton.ImageButtonStyle styleIzquierda = new ImageButton.ImageButtonStyle();
+        styleIzquierda.imageUp = new TextureRegionDrawable(new TextureRegion(buttonTextureIzquierda));
+        imageButtonIzquierda = new ImageButton(styleIzquierda);
+
+        ImageButton.ImageButtonStyle styleArriba = new ImageButton.ImageButtonStyle();
+        styleArriba.imageUp = new TextureRegionDrawable(new TextureRegion(buttonTextureArriba));
+        imageButtonArriba = new ImageButton(styleArriba);
+
+        ImageButton.ImageButtonStyle styleAbajo = new ImageButton.ImageButtonStyle();
+        styleAbajo.imageUp = new TextureRegionDrawable(new TextureRegion(buttonTextureAbajo));
+        imageButtonAbajo = new ImageButton(styleAbajo);
+
+        // Obtén las dimensiones de la pantalla
+        float screenWidth = Gdx.graphics.getWidth();
+        float screenHeight = Gdx.graphics.getHeight();
+
+        stage.getViewport().getCamera().position.set(screenWidth / 2f,0,0);
+
+
+        // TODO los eventos de los botones no coinciden con donde estan los botones en pantalla
+        // Asignación de posición y tamaño acada botón
+        imageButtonDerecha.setHeight(screenHeight * 0.15f);
+        imageButtonDerecha.setWidth(screenWidth * 0.15f);
+        imageButtonDerecha.setWidth(screenWidth * 0.15f);
+        imageButtonDerecha.setPosition(17 * screenWidth / 20f, -screenHeight / 3f);
+
+        imageButtonIzquierda.setHeight(screenHeight * 0.15f);
+        imageButtonIzquierda.setWidth(screenWidth * 0.15f);
+        imageButtonIzquierda.setPosition(14 * screenWidth / 20f, -screenHeight / 3f);
+
+        imageButtonArriba.setHeight(screenHeight * 0.15f);
+        imageButtonArriba.setWidth(screenWidth * 0.15f);
+        imageButtonArriba.setPosition(1 * screenWidth / 20f, -screenHeight / 5f);
+
+        imageButtonAbajo.setHeight(screenHeight * 0.15f);
+        imageButtonAbajo.setWidth(screenWidth * 0.15f);
+        imageButtonAbajo.setPosition(1 * screenWidth / 20f, -screenHeight / 2.5f);
+
+
+
+        // Añadimos al Stage cada botón
+        stage.addActor(imageButtonDerecha);
+        stage.addActor(imageButtonIzquierda);
+        stage.addActor(imageButtonArriba);
+        stage.addActor(imageButtonAbajo);
+
     }
 }
